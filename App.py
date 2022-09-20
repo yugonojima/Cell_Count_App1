6f93ac0eebb00449a7cd1680c10849418932efc6
yugonojima <67179342+yugonojima@users.noreply.github.com>
2022-09-20 13:49:45 +0900

    confirm stramlit movement

--- App.py
+++ App.py
@@ -44,52 +44,52 @@ def main():
 
     
 
-    if uploaded_files is not None:
-        # Make temp file path from uploaded file
-        # for uploaded_file in uploaded_files:
-        #     image=Image.open(uploaded_file)
-        #     img_array = np.array(image)
-        #     resize_img = cv2.resize(img_array, (640, 640))
-        #     img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2RGB)
-        #     cv2.imwrite('/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/test0/img{}.jpg'.format(num), img)
-        #     num += 1
+    # if uploaded_files is not None:
+    #     # Make temp file path from uploaded file
+    #     # for uploaded_file in uploaded_files:
+    #     #     image=Image.open(uploaded_file)
+    #     #     img_array = np.array(image)
+    #     #     resize_img = cv2.resize(img_array, (640, 640))
+    #     #     img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2RGB)
+    #     #     cv2.imwrite('/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/test0/img{}.jpg'.format(num), img)
+    #     #     num += 1
         
       
-        # detect2.run(source='/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/test0',
-        #  weights='/Users/yugonojima/practice/plivate/Cell-Count/yolov5/runs/train/exp2/weights/best.pt',
-        #   name='/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/result',
-        #   exist_ok=True)
+    #     # detect2.run(source='/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/test0',
+    #     #  weights='/Users/yugonojima/practice/plivate/Cell-Count/yolov5/runs/train/exp2/weights/best.pt',
+    #     #   name='/Users/yugonojima/practice/plivate/Cell-Count/cell-count-data/result',
+    #     #   exist_ok=True)
         
-        # files = glob.glob("cell-count-data/result/*")
-        # for file in files:
-        #  result_image=Image.open(file) 
-        # st.image(result_image,caption = 'サムネイル画像')
-        image_dir = "cell-count-data/result"
-        fName_list = os.listdir(image_dir)# 画像ファイルのリストを取得
-        img_file_num = len(os.listdir(image_dir))#画像ファイル数
+    #     # files = glob.glob("cell-count-data/result/*")
+    #     # for file in files:
+    #     #  result_image=Image.open(file) 
+    #     # st.image(result_image,caption = 'サムネイル画像')
+    #     image_dir = "cell-count-data/result"
+    #     fName_list = os.listdir(image_dir)# 画像ファイルのリストを取得
+    #     img_file_num = len(os.listdir(image_dir))#画像ファイル数
 
-        idx = 0
+    #     idx = 0
 
-        for _ in range(len(fName_list)-1):
-            cols = st.columns(2)
+    #     for _ in range(len(fName_list)-1):
+    #         cols = st.columns(2)
 
-            if idx < len(fName_list):
-                cols[0].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
-                print(os.path.join(image_dir, fName_list[idx]))
-                idx += 1
-            if idx < len(fName_list):
-                cols[1].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
-                idx += 1
+    #         if idx < len(fName_list):
+    #             cols[0].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
+    #             print(os.path.join(image_dir, fName_list[idx]))
+    #             idx += 1
+    #         if idx < len(fName_list):
+    #             cols[1].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
+    #             idx += 1
             
-            else:
-                break
+    #         else:
+    #             break
     
-    if st.button("削除"):
-        shutil.rmtree('cell-count-data/result/')
-        shutil.rmtree('cell-count-data/test0/')
-        os.mkdir('cell-count-data/result/')
-        os.mkdir('cell-count-data/test0/')
-    st.write('※削除ボタンを押した後、ページを更新してください')
+    # if st.button("削除"):
+    #     shutil.rmtree('cell-count-data/result/')
+    #     shutil.rmtree('cell-count-data/test0/')
+    #     os.mkdir('cell-count-data/result/')
+    #     os.mkdir('cell-count-data/test0/')
+    # st.write('※削除ボタンを押した後、ページを更新してください')
 
 
 if __name__ == "__main__":
