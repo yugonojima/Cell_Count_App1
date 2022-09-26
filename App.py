939143882821f04646eca11a559354fd4cd2c6ce
yugonojima <67179342+yugonojima@users.noreply.github.com>
2022-09-26 15:43:27 +0900

    use app temporary

--- App.py
+++ App.py
@@ -45,10 +45,10 @@ def main():
     if uploaded_files == [] :
         uploaded_files = None
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-    img = cv2.imread('cell-count-data/test0/img0.jpg')
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    # img = cv2.imread('cell-count-data/test0/img0.jpg')
 
-    result = model(img)
+    # result = model(img)
     # st.image(img)
    #なんでこれがエラーになるんか、、、
 
@@ -61,18 +61,18 @@ def main():
             img_array = np.array(image)
             resize_img = cv2.resize(img_array, (640, 640))
             
-            # img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2RGB)
+            img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2RGB)
             
-            # input_image.append(img)
-            # cv2.imwrite('cell-count-data/test0/img{}.jpg'.format(num), img)
-            # num += 1
+            input_image.append(img)
+            cv2.imwrite('cell-count-data/test0/img{}.jpg'.format(num), img)
+            num += 1
         
         
       
-        # detect2.run(source='cell-count-data/test0',
-        #  weights='yolov5/runs/train/exp2/weights/best.pt',
-        #   name='cell-count-data/result',
-        #   exist_ok=True)
+        detect2.run(source='cell-count-data/test0',
+         weights='yolov5/runs/train/exp2/weights/best.pt',
+          name='cell-count-data/result',
+          exist_ok=True)
         
         # files = glob.glob("cell-count-data/result/*")
         # for file in files:
@@ -80,25 +80,25 @@ def main():
         # st.image(result_image,caption = 'サムネイル画像')
 
 
-        # image_dir = "cell-count-data/result"
-        # fName_list = os.listdir(image_dir)# 画像ファイルのリストを取得
-        # img_file_num = len(os.listdir(image_dir))#画像ファイル数
+        image_dir = "cell-count-data/result"
+        fName_list = os.listdir(image_dir)# 画像ファイルのリストを取得
+        img_file_num = len(os.listdir(image_dir))#画像ファイル数
 
-        # idx = 0
+        idx = 0
 
-        # for _ in range(len(fName_list)-1):
-        #     cols = st.columns(2)
+        for _ in range(len(fName_list)-1):
+            cols = st.columns(2)
 
-        #     if idx < len(fName_list):
-        #         cols[0].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
-        #         print(os.path.join(image_dir, fName_list[idx]))
-        #         idx += 1
-        #     if idx < len(fName_list):
-        #         cols[1].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
-        #         idx += 1
+            if idx < len(fName_list):
+                cols[0].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
+                print(os.path.join(image_dir, fName_list[idx]))
+                idx += 1
+            if idx < len(fName_list):
+                cols[1].image(f'./cell-count-data/result/{fName_list[idx]}', caption=fName_list[idx])
+                idx += 1
             
-        #     else:
-        #         break
+            else:
+                break
     
     if st.button("削除"):
         shutil.rmtree('cell-count-data/result/')
